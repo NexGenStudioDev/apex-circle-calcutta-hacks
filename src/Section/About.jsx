976ff37de0a6dfccbad4ac08373bc55157d99ef0
Anjali Tomar <anjali.theaj3366@gmail.com>
2025-10-22 17:51:51 +0530

    add img and icon & change socialIcons

--- src/Section/About.jsx
+++ src/Section/About.jsx
@@ -44,16 +44,32 @@ const About = () => (
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-8 ">
         {[
-          { imageSrc: FaTram, label: 'Historic Trams', subtitle: 'First in Asia' },
-          { imageSrc: FaBus, label: 'Blue Buses', subtitle: 'City Symbol' },
-          { imageSrc: FaTaxi, label: 'Yellow Taxis', subtitle: 'Iconic Rides' },
-          { imageSrc: FaArchway, label: 'Howrah Bridge', subtitle: 'Engineering Marvel' },
+          {
+            imageSrc: '../src/assets/Icon/Tram.jpg',
+            label: 'Historic Trams',
+            subtitle: 'First in Asia',
+          },
+          {
+            imageSrc: '../src/assets/Icon/bluebus.png',
+            label: 'Blue Buses',
+            subtitle: 'City Symbol',
+          },
+          {
+            imageSrc: '../src/assets/Icon/Taxi.png',
+            label: 'Yellow Taxis',
+            subtitle: 'Iconic Rides',
+          },
+          {
+            imageSrc: '../src/assets/Icon/Bridge.jpg',
+            label: 'Howrah Bridge',
+            subtitle: 'Engineering Marvel',
+          },
         ].map((item, idx) => (
           <div
             key={idx}
             className="text-center bg-[#F4E5C2] p-6 border-2 border-[#3E2C1D] shadow-md aged-paper"
           >
-           <img src="/src/assets/Icon/bluebus.png" alt="" srcset="" className='size-20' />
+            <img src={item.imageSrc} alt="" srcset="" className="size-20" />
             <div className="font-display font-bold text-[#3E2C1D]">{item.label}</div>
             <div className="font-serif text-sm text-[#6B4423] italic">{item.subtitle}</div>
           </div>
