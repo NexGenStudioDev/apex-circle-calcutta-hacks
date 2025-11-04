a0d5d8c8e30fd8b645f20782a6f1b25ad4ad34f0
Pritam Das <pritamdas.pd2005@gmail.com>
2025-11-04 20:08:05 +0530

    Down souvik no.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -218,19 +218,19 @@ const Footer = () => {
               </a>
 
               <a
-                href="tel:+917003058672"
-                className="flex items-center hover:text-[#D4AF37] transition-colors font-serif"
-              >
-                <Phone size={16} className="text-[#D4AF37] mr-[0.5rem]" />
-                <span>+91 70030 58672</span>
-              </a>
-
-              <a
                 href="tel:+916291129896"
                 className="flex items-center hover:text-[#D4AF37] transition-colors font-serif"
               >
                 <Phone size={16} className="text-[#D4AF37] mr-[0.5rem]" />
                 <span>+91 62911 29896</span>
+              </a>
+
+              <a
+                href="tel:+917003058672"
+                className="flex items-center hover:text-[#D4AF37] transition-colors font-serif"
+              >
+                <Phone size={16} className="text-[#D4AF37] mr-[0.5rem]" />
+                <span>+91 70030 58672</span>
               </a>
             </div>
           </div>
